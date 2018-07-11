291670074929a8ae020d6187f6c8325bd27af4d2
Bobby Martin and Garnett Puett <bobby@>
2018-07-11 13:41:28 -0700

    Add all components and make base React app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Game from './Game';
 
 
 function App(){
   return (
     <div>
       <Switch>
-        
+        <Route exact path='/' component={Game}/>
       </Switch>
     </div>
   );
